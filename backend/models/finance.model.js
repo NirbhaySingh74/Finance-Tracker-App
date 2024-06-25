9f2f99628acfec835fe9678860118d7cdf2f9c6d
Nirbhay <nirbhaysingh943012@gmail.com>
2024-06-25 14:56:04 +0530

    finance model created!

---
+++ backend/models/finance.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const financeSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const FinanceModle = mongoose.model("FinanceModle", financeSchema);
